98c77e9b1f8a7003ada730b0323d1db2ae8e670c
mschaller2 <mschaller2@wisc.edu>
2018-03-02 17:45:20 -0600

    Initial commit

---
+++ app/src/main/java/com/example/mitch/myapplication2/MainActivity.java
@@ -0,0 +1,68 @@
+package com.example.mitch.myapplication2;
+
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    private int counter = 0;
+    public static final String EXTRA_MESSAGE = "com.example.mitch.myapplication2.MESSAGE";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button playCarl = (Button) this.findViewById(R.id.button2);
+        Button stopAll = (Button) this.findViewById(R.id.button3);
+        Button playSlay = (Button) this.findViewById(R.id.button4);
+
+        final MediaPlayer mp = MediaPlayer.create(this, R.raw.carl_redbone);
+        final MediaPlayer mp3 = MediaPlayer.create(this, R.raw.slayer_song);
+
+        playSlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (counter == 0) {
+                    mp3.start();
+                }
+            }
+        });
+        playCarl.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (counter == 0) {
+                    mp.start();
+                }
+            }
+        });
+
+        stopAll.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mp.isPlaying()) {
+                    mp.pause();
+                }
+                if (mp3.isPlaying()) {
+                    mp3.pause();
+                }
+            }
+        });
+
+    }
+
+    // called when user taps Send
+    public void sendMessage(View view){
+
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText)findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+
+    }
+}
